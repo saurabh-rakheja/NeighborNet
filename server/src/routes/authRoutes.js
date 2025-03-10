f7aeadbe32d7e0670bdd54ff7b3fedfdd3860a89
coderhisham <imhish4m@gmail.com>
2025-03-10 13:59:39 +0530

    Added Dashboard page, Complete Auth Implementation, Customized Navbar

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,104 +1,31 @@
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
-const crypto = require("crypto");
+const express = require("express");
+const router = express.Router();
+const { body } = require("express-validator");
+const authController = require("../controllers/authContoller");
 
-const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, "Please provide a name"],
-      trim: true,
-      maxLength: [100, "Name cannot exceed 100 characters"],
-    },
-    email: {
-      type: String,
-      required: [true, "Please provide an email"],
-      unique: true,
-      trim: true,
-      lowercase: true,
-      match: [/^\S+@\S+\.\S+$/, "Please provide a valid email"],
-      maxLength: [100, "Email cannot exceed 100 characters"],
-    },
-    password: {
-      type: String,
-      required: [true, "Please provide a password"],
-      minLength: [6, "Password cannot be less than 6 characters"],
-      select: false,
-    },
-    role: {
-      type: String,
-      default: "user",
-      enum: {
-        values: ["user", "admin", "farmer", "distributor", "retailer"],
-        message: "Please select a valid role",
-      },
-    },
-    tokenVersion: {
-      type: Number,
-      default: 0,
-    },
-    loginAttempts: {
-      type: Number,
-      default: 0,
-    },
-    lockUntil: {
-      type: Number,
-      default: 0,
-    },
-    passwordChangedAt: Date,
-    passwordResetToken: String,
-    passwordResetExpires: Date,
-    active: {
-      type: Boolean,
-      default: true,
-      select: false,
-    },
-  },
-  { timestamps: true }
+// Public routes
+router.post(
+  "/register",
+  [
+    body("name", "Name is required").not().isEmpty(),
+    body("email", "Please include a valid email").isEmail(),
+    body("password", "Password must be at least 6 characters").isLength({
+      min: 6,
+    }),
+  ],
+  authController.register
 );
 
-// Hash password before saving
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+router.post(
+  "/login",
+  [
+    body("email", "Please include a valid email").isEmail(),
+    body("password", "Password is required").exists(),
+  ],
+  authController.login
+);
 
-  try {
-    const salt = await bcrypt.genSalt(12);
-    this.password = await bcrypt.hash(this.password, salt);
-    this.passwordChangedAt = Date.now() - 1000;
-    next();
-  } catch (error) {
-    next(error);
-  }
-});
+router.post("/forgot-password", authController.forgotPassword);
+router.patch("/reset-password/:token", authController.resetPassword);
 
-// Compare password
-userSchema.methods.comparePassword = async function (candidatePassword) {
-  try {
-    return await bcrypt.compare(candidatePassword, this.password);
-  } catch (error) {
-    console.error("Error comparing passwords:", error);
-    throw new Error("Error comparing passwords");
-  }
-};
-
-// Generate password reset token
-userSchema.methods.createPasswordResetToken = function () {
-  const resetToken = crypto.randomBytes(32).toString("hex");
-
-  this.passwordResetToken = crypto
-    .createHash("sha256")
-    .update(resetToken)
-    .digest("hex");
-
-  this.passwordResetExpires = Date.now() + 10 * 60 * 1000; // 10 minutes
-
-  return resetToken;
-};
-
-// Exclude inactive users from query results
-userSchema.pre(/^find/, function (next) {
-  this.find({ active: { $ne: false } });
-  next();
-});
-
-module.exports = mongoose.model("User", userSchema);
+module.exports = router;
